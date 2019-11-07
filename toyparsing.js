5b7bd8bb8e21c7b6ae78060025d9832b6a82ff6f
Satoshi Okui <okuisatoshi@mac.com>
2019-11-07 11:31:19 +0900

    Update dependent library

--- toyparsing.js
+++ toyparsing.js
@@ -8,121 +8,121 @@ class ParserFailure extends Error {}
 
 
 const parser_do = function(do_func) {
-    const _parser = function(s) {
-	const run = function(m, opts) {
-	    const nullable  = opts && opts.nullable  || false;
-	    const throwable = opts && opts.throwable || false;	    
-	    const r = m(s);
-	    if (typeof r !== 'undefined') {
-		s = r.s;
-		return r.a;
+    const parser = function(s) {
+	    const run = function(m, opts) {
+	        const nullable  = opts && opts.nullable  || false;
+	        const throwable = opts && opts.throwable || false;
+	        const r = m(s);
+	        if (typeof r !== 'undefined') {
+                s = r.s;
+                return r.a;
+	        }
+	        if (nullable) return undefined;
+	        if (throwable) throw new ParserFailure();
+	        throw new _Undefined();
+	    };
+
+	    try {
+	        return {a: do_func(run), s: s};
 	    }
-	    if (nullable) return undefined;
-	    if (throwable) throw new ParserFailure(); 
-	    throw new _Undefined();
-	};
-	try {
-	    return {a:_parser.action(do_func(run)),s:s};
-	}
-	catch (e) {
-	    if (e instanceof _Undefined) return undefined;
-    	    throw e;
-	}
+	    catch (e) {
+	        if (e instanceof _Undefined) return undefined;
+            throw e;
+	    }
     };
-    _parser.action = x => x;
-    return _parser;
+
+    parser.bind = function(f) {
+        return parser_do(run => run(f(run(this))));
+    };
+
+    parser.fmap = function(f) {
+        return parser_do(run => f(run(this)));
+    };
+
+    return parser;
 };
-
-
-const bind = (m,f) => 
-      parser_do(run => run(f(run(m))));
 
 
 const unit = a => parser_do(run => a);
 
 
-const get = s => ({a:s,s:s});
+const get = s => ({a: s, s: s});
 
 
-const put = s => _ => ({a:s,s:s});
+const put = s => _ => ({a: s, s: s});
 
 
 const fail = _ => undefined;
 
 
-const silent = p => bind(p, _ => unit([])); // Attach silent action
+const empty = unit([]);
 
 
-const newParser = p => bind(p, unit);
-
-      
-const toParser = p => typeof p === "string" ? silent(word(p)) : p;
+const toParser = p => typeof p === "string" ? word(p).fmap(_ => []) : p;
 
 
 const word = w =>
-    parser_do(run => {  
+    parser_do(run => {
         let s = run(get);
         const n = s.match(/^\s*/)[0].length;
         s = s.slice(n)
         if (s.slice(0,w.length) === w) {
-	    run(put(s.slice(w.length)));
-	    return w;
-	}    
+            run(put(s.slice(w.length)));
+            return w;
+        }
         run(fail);
     });
 
 
 const pattern = re =>
     parser_do(run => {
-	let s = run(get);  
+	let s = run(get);
         const n = s.match(/^\s*/)[0].length;
         s = s.slice(n)
         const r = s.match(new RegExp('^' + re))
         if (r) {
-	    const w = r[0]
-	    run(put(s.slice(w.length))); 
-	    return w;
-	}
+            const w = r[0]
+            run(put(s.slice(w.length)));
+            return w;
+        }
         run(fail);
     });
 
 
-const empty = unit([]); 
-
-
-const cat = (p1,p2) =>
+const cat = (p1, p2) =>
     parser_do(run => {
         let a1 = run(toParser(p1));
-	if (!(a1 instanceof Array)) a1 = [a1];
-	let a2 = run(toParser(p2));
-	if (!(a2 instanceof Array)) a2 = [a2];
+        if (!(a1 instanceof Array)) a1 = [a1];
+        let a2 = run(toParser(p2));
+        if (!(a2 instanceof Array)) a2 = [a2];
         return a1.concat(a2);
-    }); 
+    });
 
 
 const seq = (...args) => args.reduce(cat,empty);
 
 
-const or = (p1,p2) =>
+const or = (p1, p2) =>
     parser_do(run => {
-        const a = run(toParser(p1),{nullable:true});
-  	if (typeof a !== 'undefined') return a;
+        const a = run(toParser(p1),{nullable: true});
+        if (typeof a !== 'undefined') return a;
         return run(toParser(p2))
     });
+
 
 const oneOf = (...args) => args.reduce(or,fail);
 
 
-const optional = p => or(p,empty);
+const optional = p => or(p, empty);
 
 
 const moreThan0 = p => {
     // Consume at least one character; fail, otherwise
     const q = parser_do(run => {
-	const s = run(get);
-	const a = run(p);
-	if (run(get).length < s.length) return a; 
-	run(fail);
+        const s = run(get);
+        const a = run(p);
+        if (run(get).length < s.length) return a;
+        run(fail);
     });
     // Need a thunk to avoid infinite recursive call
     const p_star = parser_do(run => run(or(cat(q,p_star),empty)));
@@ -135,3 +135,18 @@ const moreThan1 = p => cat(p,moreThan0(p));
 
 const sepBy = (p,sep) => cat(p,moreThan0(cat(sep,p)));
 
+
+exports.ParserFailure = ParserFailure;
+exports.parser_do = parser_do;
+exports.get = get;
+exports.put = put;
+exports.pattern = exports.pat = pattern;
+exports.word = exports.w = word;
+exports.fail = fail;
+exports.empty = empty;
+exports.seq = seq;
+exports.oneOf = exports.one_of = oneOf;
+exports.optional = exports.opt = optional;
+exports.moreThan0 = exports.for_at_least0 = moreThan0;
+exports.moreThan1 = exports.for_at_least1 = moreThan1;
+exports.sepBy = exports.sep_by = sepBy;
